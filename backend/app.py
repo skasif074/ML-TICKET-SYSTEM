465d21505992b36d936e312e9305883a86474fc5
skasif074 <asifgaming074@gmail.com>
2026-09-25 16:53:04 +0530

    Updated Middleware

--- backend/app.py
+++ backend/app.py
@@ -22,7 +22,11 @@ app = FastAPI(title="Customer Support Ticket Classifier API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://ml-ticket-system.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
